2f58b419c80423cc4852c71b3e958e19351a957f
Aranciata99 <basil.knill@zhdk.ch>
2025-10-08 13:46:32 +0200

    Cwodfounding Eng

--- site/snippets/sticker.php
+++ site/snippets/sticker.php
@@ -1,7 +1,7 @@
 <a href="<?= $pages = $site->find('crowdfunding') ?>" style="font-style: normal;">
     <div class="crowdfoundingSticker">
         <h2><span class="helvetica">♥ ♥ ♥</span><br>
-            Unterstütze uns!<br>
+            <?= page('crowdfunding')?->sticker() ?><br>
             ★★★</h2>
         </h2>
     </div>
